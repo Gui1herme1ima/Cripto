584cb372049fad26ae9e08d588c41fcf7dcbd117
Lima <guilimam164@gmail.com>
2024-05-24 13:37:57 -0300

    Base do projeto

---
+++ Main.py
@@ -0,0 +1,8 @@
+import Alfabeto
+
+entrada = input("Digite o que deseja criptografar: ")
+
+entradaConvEmChar = list(entrada)
+
+if entradaConvEmChar == Alfabeto.teste:
+    print("deu bom rapeize")
---
+++ Matriz.py
@@ -0,0 +1,3 @@
+#(Primeira letra, Segunda letra, terceira letra)
+#(Quant. vogais, Letra c/ maior valor, Quant. Consoantes)
+#(AntiPenultima letra, Penúltima Letra , Última letra)
