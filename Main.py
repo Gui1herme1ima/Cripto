e6cbd9394823ecccb7c7c8319c2ceace50a37da5
Lima <130940527+Gui1herme1ima@users.noreply.github.com>
2024-05-27 19:15:58 -0300

    Matriz - OK

--- Main.py
+++ Main.py
@@ -1,44 +1,65 @@
 import Alfabeto
 import Matriz
-def posicao_no_alfabeto(lista):
-    # Convertemos a letra para maiúscula para lidar com maiúsculas e minúsculas de forma uniforme
+
+
+def posicaoNoAlfabeto(lista):
+
     listaPosicao = []
     for char in lista:
         char = char.upper()
 
         if char.isdigit():
             listaPosicao.append(int(char))
-        elif char in {',','.',';',':','(',')','{','}','[',']','@','#','$','%','&','*','?','!','_','=','+','-'}:
+        elif char in {',', '.', ';', ':', '(', ')', '{', '}', '[', ']', '@', '#', '$', '%', '&', '*', '?', '!', '_',
+                      '=', '+', '-', '"'}:
             listaPosicao.append(0)
         elif char == 'Ç':
             listaPosicao.append(3)
         else:
-            for i in range(26):  # Existem 26 letras no alfabeto inglês
+            for i in range(26):
                 letra_atual = chr(ord('A') + i)
                 if char == letra_atual:
-                    listaPosicao.append(i+1)
+                    listaPosicao.append(i + 1)
 
     return listaPosicao
 
+
 contadorVogais = 0
 contadorConsoantes = 0
+
+
 def verificarVogaisEConsoantes(lista):
     global contadorVogais
     global contadorConsoantes
     for char in lista:
         char = char.upper()
-        if char in {',', '.', ';', ':', '(', ')', '{', '}', '[', ']', ' ', 'Ç', '@','#','$','%','&','*','?','!','_','=','+','-'}:
+        if char in {',', '.', ';', ':', '(', ')', '{', '}', '[', ']', ' ', 'Ç', '@', '#', '$', '%', '&', '*', '?', '!',
+                    '_', '=', '+', '-', '"'}:
             break
-        elif char in {'A','E','I','O','U'}:
+        elif char in {'A', 'E', 'I', 'O', 'U'}:
             contadorVogais = contadorVogais + 1
         else:
             contadorConsoantes = contadorConsoantes + 1
 
 
+def verificarMaiorLetra(lista):
+    maiorLetra = max(lista, key=lambda letra: letra.upper())
+    maiorLetra = maiorLetra.upper()
+    posicaoMaiorLetra = ord(maiorLetra) - ord('A') + 1
+    return posicaoMaiorLetra
+
 
 if __name__ == "__main__":
+
     entrada = input("Digite o que deseja criptografar: ")
     entradaConvEmChar = list(entrada)
+    if len(entradaConvEmChar) < 2:
+        entradaConvEmChar.append('0')
+        entradaConvEmChar.append('0')
+    elif len(entradaConvEmChar) < 3:
+        entradaConvEmChar.append('0')
+
     verificarVogaisEConsoantes(entradaConvEmChar)
-    Matriz.recebeLista(posicao_no_alfabeto(entradaConvEmChar), contadorVogais, contadorConsoantes)
+    Matriz.recebeLista(posicaoNoAlfabeto(entradaConvEmChar), contadorVogais, contadorConsoantes,
+                       verificarMaiorLetra(entradaConvEmChar))
     Matriz.imprimirMatriz()
--- Matriz.py
+++ Matriz.py
@@ -8,16 +8,18 @@ matrizCripto = [
     [0, 0, 0]
 ]
 
-def recebeLista(lista, quantVog, quantCons):
-    print(quantVog)
-    print(quantCons)
-    matrizCripto[0][0] = int(lista[0]) #Primeira Letra
-    matrizCripto[0][1] = int(lista[1]) #Segunda Letra
-    matrizCripto[0][2] = int(lista[2]) #Terceira Letra
 
+def recebeLista(lista, quantVog, quantCons, MaiorLetra):
+    matrizCripto[0][0] = int(lista[0])  # Primeira Letra
+    matrizCripto[0][1] = int(lista[1])  # Segunda Letra
+    matrizCripto[0][2] = int(lista[2])  # Terceira Letra
+    matrizCripto[1][0] = int(quantVog)  # Quantidade Vogais
+    matrizCripto[1][1] = int(MaiorLetra)  # Maior Letra
+    matrizCripto[1][2] = int(quantCons)  # Quantidade Consoantes
     matrizCripto[2][0] = int(lista[-3])  # AntiPenúltima Letra
     matrizCripto[2][1] = int(lista[-2])  # Penúltima Letra
     matrizCripto[2][2] = int(lista[-1])  # Última Letra
+
 
 def imprimirMatriz():
     print(matrizCripto)
