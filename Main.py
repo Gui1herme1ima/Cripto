0586556e8c79a1126dc1613f5ac9bc0b94fc078b
Lima <130940527+Gui1herme1ima@users.noreply.github.com>
2024-05-27 00:45:00 -0300

    Base

--- Main.py
+++ Main.py
@@ -1,8 +1,35 @@
 import Alfabeto
+def posicao_no_alfabeto(lista):
+    # Convertemos a letra para maiúscula para lidar com maiúsculas e minúsculas de forma uniforme
+    listaPosicao = []
+    for char in lista:
+        char = char.upper()
+
+        if char.isdigit():
+            listaPosicao.append(int(char))
+        elif char == ',' or '.' or ';' or ':' or '(' or ')' or '{' or '}' or '[' or ']':
+            listaPosicao.append(0)
+        elif char == 'ç' or 'Ç':
+            listaPosicao.append(3)
+        else:
+            for i in range(26):  # Existem 26 letras no alfabeto inglês
+                letra_atual = chr(ord('A') + i)
+                if char == letra_atual:
+                    listaPosicao.append(i+1)
+
+    print(listaPosicao)
+
+def processar_lista_de_chars(lista):
+    listaPosicao = []
+    for char in lista:
+        listaPosicao.append(posicao_no_alfabeto(char))
+    print(listaPosicao)
+
+
+
 
 entrada = input("Digite o que deseja criptografar: ")
 
 entradaConvEmChar = list(entrada)
 
-if entradaConvEmChar == Alfabeto.teste:
-    print("deu bom rapeize")
+posicao_no_alfabeto(entradaConvEmChar)
--- Matriz.py
+++ Matriz.py
@@ -1,3 +1,15 @@
 #(Primeira letra, Segunda letra, terceira letra)
 #(Quant. vogais, Letra c/ maior valor, Quant. Consoantes)
 #(AntiPenultima letra, Penúltima Letra , Última letra)
+
+from Main import entradaConvEmChar
+primeiraLetra=0; segundaLetra=1; terceiraLetra=2
+quantVogais=3; letraMaiorVal=4; quantConsoantes=5
+antPenultimaLetra=6; penultimaLetra=7; ultimaLetra=8;
+
+matrizCripto = [
+    [primeiraLetra, segundaLetra, terceiraLetra],
+    [quantVogais, letraMaiorVal, quantConsoantes],
+    [antPenultimaLetra, penultimaLetra, ultimaLetra]
+]
+
