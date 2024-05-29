58507a2047eb89a5b595188621b03de1f9377bc3
Lima <guilimam164@gmail.com>
2024-05-29 16:28:32 -0300

    Descriptografia v.1.1

--- Descripto.py
+++ Descripto.py
@@ -1,5 +1,15 @@
+import Alfabeto
+
 def PalavraCriptografada(palavraCriptografada):
     palavraCriptografada = palavraCriptografada
 
 def Chave(chave):
-    chave = chave
+    partes = chave.split('/')
+    codigo = partes[0]
+    determinante = int(partes[1])
+
+    verificarChave(codigo)
+def verificarChave(codigo):
+
+    if codigo in Alfabeto.codigos0:
+        print("deu bom")
